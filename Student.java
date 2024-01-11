90546f80d9209fe95918d3d0eb14d8c817f0dedf
abhilashreddyjagirapu <131015623+abhilashreddyjagirapu@users.noreply.github.com>
2024-01-11 13:57:33 -0600

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+	String name="Jagirapu";
+	String getName()
+	{
+		return name;
+	}
+
+	public static void main(String[] args) {
+		Student s1=new Student();
+		System.out.println(s1.getName());
+		// TODO Auto-generated method stub
+
+	}
+
+}
