9242fb92aa77069819ea322fa3998b35217274be
abhilashreddyjagirapu <abhilash4144@gmail.com>
2024-01-18 12:05:08 -0600

    arrays

---
+++ CISAssignments/arrays/identifyLength.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class identifyLength {
+public static void main(String[] args) {
+		
+		int a[] = new int[10];
+		a[0]=10;
+		a[1]=2;
+		
+		System.out.println(a.length);
+	}
+
+
+}
