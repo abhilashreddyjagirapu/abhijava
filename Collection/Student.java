831f12097c0b763790fe363acf65dae95522e2f7
abhilashreddyjagirapu <abhilash4144@gmail.com>
2024-01-30 14:28:40 -0600

    added new files

---
+++ Collection/Student.java
@@ -0,0 +1,50 @@
+package Collection;
+import java.util.Objects;
+public class Student {
+	private String firstName;
+    private String lastName;
+    private double gpa;
+
+    // Constructors, getters, setters (not shown for brevity)
+    public Student(String firstName, String lastName, double gpa) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gpa = gpa;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Double.compare(student.gpa, gpa) == 0 &&
+                Objects.equals(firstName, student.firstName) &&
+                Objects.equals(lastName, student.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, gpa);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gpa=" + gpa +
+                '}';
+    }
+}
