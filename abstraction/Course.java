9768988f164369eee77363667c33c8c226c8bd90
abhilashreddyjagirapu <abhilash4144@gmail.com>
2024-02-07 15:44:24 -0600

    push on 7thfeb

--- abstraction/Course.java
+++
@@ -1,9 +0,0 @@
-package abstraction;
-
-public class Course extends Department{
-
-    @Override
-    int getDepartmentSize() {
-        return 20;
-    }
-}
--- abstraction/Main.java
+++
@@ -1,8 +0,0 @@
-package abstraction;
-
-public class Main {
-	public static void main(String[] args) {
-        Course course = new Course();
-        System.out.println(course.getDepartmentSize());
-    }
-}
